cf024c62f1c4440d7ac7f2ad1a8fa8644638f264
DivadNojnarg <dgranjon@ymail.com>
2023-07-21 17:24:14 +0200

    get file list

--- R/utils.R
+++ R/utils.R
@@ -1,9 +1,7 @@
 #'@keywords internal
 copy_assets <- function(type, copy_path) {
-  files_path <- system.file(type, package = "webR4Shiny")
-  lapply(seq_along(files_path), function(i) {
-    file.copy(files_path[[i]], copy_path)
-  })
+  files_path <- list.files(system.file(type, package = "webR4Shiny"))
+  file.copy(from = files_path, to = copy_path, recursive = TRUE)
 }
 
 #'@keywords internal
